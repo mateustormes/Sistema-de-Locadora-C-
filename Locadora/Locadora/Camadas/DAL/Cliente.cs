9bb94b2d1747b6db725efe221b2ef2634a70bea8
Mateus Tormes <mateus.tormes@hotmail.com>
2022-10-15 23:06:46 -0300

    first commit

---
+++ Locadora/Locadora/Camadas/DAL/Cliente.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Locadora.Camadas.DAL
+{
+    public class Cliente
+    {
+        private string strCon = Conexao.getConexao(); 
+
+        public List<MODEL.Cliente> Select()
+        {
+            List<MODEL.Cliente> lstClientes = new List<MODEL.Cliente>();
+            SqlConnection conexao = new SqlConnection(strCon);
+            string sql = "Select * from Clientes";
+            SqlCommand cmd = new SqlCommand(sql, conexao);
+            conexao.Open();
+            try
+            {
+                SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection); 
+                while (reader.Read())
+                {
+                    MODEL.Cliente cliente = new MODEL.Cliente();
+                    cliente.id = Convert.ToInt32(reader[0]);
+                    cliente.nome = reader["nome"].ToString();
+                    cliente.endereco = reader["endereco"].ToString();
+                    cliente.cidade = reader["cidade"].ToString();
+                    cliente.estado = reader["estado"].ToString();
+                    cliente.aniversario = Convert.ToDateTime(reader["aniversario"]);
+                    lstClientes.Add(cliente); 
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Deu erro na Seleção de Clientes...");
+            }
+            finally
+            {
+                conexao.Close(); 
+            }
+
+            return lstClientes; 
+        }
+
+    }
+}
